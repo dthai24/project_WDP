df7bfe3668e95ce406b3679db2cc076037dcad57
nhatminh-k18hl <minhnnhe186934@fpt.edu.vn>
2026-06-01 23:19:19 +0700

    Change .gitignore
    Change gitignore make it don't claim node_modules to push anymore

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import AdminRoutes from "./routes/admin-routes";
+import HomePage from "./pages/Home/HomePage";
 
 function App() {
-  const [currentUser, setCurrentUser] = useState(null);
-
-  useEffect(() => {
-    const savedUser = localStorage.getItem("learnpath_user");
-    if (savedUser) {
-      try {
-        setCurrentUser(JSON.parse(savedUser));
-      } catch (error) {
-        console.error("Error parsing saved user session:", error);
-        localStorage.removeItem("learnpath_user");
-      }
-    }
-  }, []);
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/admin/*" element={<AdminRoutes />} />
-
-        <Route path="*" element={<Navigate to="/admin" replace />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <HomePage />;
 }
 
 export default App;
