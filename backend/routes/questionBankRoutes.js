06d935218bf7370ef77cc93babcbf0c3a56b8284
hunghqhe180380-hub <hunghqhe180380@fpt.edu.vn>
2026-06-29 13:33:31 +0700

    Fix (Mentor) view list question bank

--- backend/routes/questionBankRoutes.js
+++ backend/routes/questionBankRoutes.js
@@ -1,40 +1,24 @@
+
 const express = require('express');
 const questionBankRoutes = express.Router();
 
 const {
-  getAllBankOfMentor,
-  getAllQuestionBankByMentorId,
-  getCoursePathBanks,
-  getPathQuestions,
-  getBankQuestions,
-  getBankPaths,
-  createPathQuestions,
-  patchPathQuestions,
-  patchQuestionActive,
-  deletePathQuestion,
-  patchAllQuestionsActive,
+    getAllBankOfMentor
 } = require('../controllers/questionBankController');
 
-const optionalAuth = (req, _res, next) => {
-  const userId = req.headers['x-user-id'] || req.query.userId;
-  if (userId) {
-    req.user = { userId: Number(userId) };
-  }
-  next();
+
+const optionalAuth = (req, res, next) => {
+    const userId = req.headers['x-user-id'] || req.query.userId;
+
+    if (userId) {
+        req.user = {
+            userId: Number(userId),
+        };
+    }
+
+    next();
 };
 
-questionBankRoutes.use(optionalAuth);
 
-questionBankRoutes.get('/getAllBankOfMentor', getAllBankOfMentor);
-questionBankRoutes.get('/getAll', getAllQuestionBankByMentorId);
-questionBankRoutes.get('/courses/:courseId/path-banks', getCoursePathBanks);
-questionBankRoutes.get('/courses/:courseId/paths/:pathId/questions', getPathQuestions);
-questionBankRoutes.post('/courses/:courseId/paths/:pathId/questions', createPathQuestions);
-questionBankRoutes.patch('/courses/:courseId/paths/:pathId/questions', patchPathQuestions);
-questionBankRoutes.patch('/courses/:courseId/paths/:pathId/questions/active-all', patchAllQuestionsActive);
-questionBankRoutes.patch('/courses/:courseId/paths/:pathId/questions/:questionId/active', patchQuestionActive);
-questionBankRoutes.delete('/courses/:courseId/paths/:pathId/questions/:questionId', deletePathQuestion);
-questionBankRoutes.get('/:bankId/paths', getBankPaths);
-questionBankRoutes.get('/:bankId/questions', getBankQuestions);
-
+questionBankRoutes.get('/getAllBankOfMentor', optionalAuth, getAllBankOfMentor);
 module.exports = questionBankRoutes;
